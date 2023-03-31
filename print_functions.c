da2adf1b9b43c7b54e8683b48b1126e5fbe69263
Edward <101046778+Eddy-96@users.noreply.github.com>
2023-03-31 22:14:23 +0200

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -4,7 +4,7 @@
 
 /**
  * print_char - Prints a char
- * by Pontsho and Edward
+ * by Pontsho and Edward.
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
