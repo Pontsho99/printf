b8492cfa6fd10d3cca5a4995e19c936fdbd7b977
Edward <101046778+Eddy-96@users.noreply.github.com>
2023-03-31 22:11:06 +0200

    Update flags.c

--- flags.c
+++ flags.c
@@ -2,7 +2,7 @@
 
 /**
  * get_flags - Calculates active flags
- * by Pontsho and Edward
+ * by Pontsho and Edward.
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
